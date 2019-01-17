c9a9e94d5fed3544d9f312fc47a523d2df59bc8d
Tim Kostka <kostka@gmail.com>
2019-01-17 18:09:43 -0500

    added valid value unit tests

--- test_ucal.py
+++ test_ucal.py
@@ -6,12 +6,39 @@ Perform unit tests on ucal.py.
 
 import unittest
 
-from ucal import evaluate, ParserError
 import ucal
 
 
 class TestSyntax(unittest.TestCase):
     """Test for correct detection of various syntax error."""
+
+    def no_exception(self, function, *args, **kwargs):
+        """Return True if no exceptions are raised, else False."""
+        passed = True
+        try:
+            function(*args, **kwargs)
+        except ucal.ParserError:
+            passed = False
+        if not passed:
+            arguments = [*args]
+            arguments.extend('%s=%s' % (x, y) for x, y in kwargs.items())
+            self.fail('%s(%s) raised an exception.'
+                      % (callable.__name__, ', '.join(arguments)))
+        return True
+
+    def test_value_comprehension(self):
+        """Test for valid value definitions"""
+        self.no_exception(ucal.evaluate, '1')
+        self.no_exception(ucal.evaluate, '123457890')
+        self.no_exception(ucal.evaluate, '001')
+        self.no_exception(ucal.evaluate, '0.1234567890')
+        self.no_exception(ucal.evaluate, '.1')
+        self.no_exception(ucal.evaluate, '+1')
+        self.no_exception(ucal.evaluate, '-1')
+        self.no_exception(ucal.evaluate, '1e9')
+        self.no_exception(ucal.evaluate, '+1e+9')
+        self.no_exception(ucal.evaluate, '-1e-9')
+        self.no_exception(ucal.evaluate, '1E6')
 
     def test_unbalanced_parentheses(self):
         """Test for unbalanced parentheses."""
@@ -31,11 +58,11 @@ class TestSyntax(unittest.TestCase):
 
     def test_implicit_multiplication(self):
         """Test for valid implicit multiplication."""
-        ucal.evaluate('1 m')
-        ucal.evaluate('m m')
-        ucal.evaluate('(1) m')
-        ucal.evaluate('(1)(1)')
+        self.no_exception(ucal.evaluate, '1 m')
+        self.no_exception(ucal.evaluate, 'm m')
+        self.no_exception(ucal.evaluate, '(1) m')
+        self.no_exception(ucal.evaluate, '(1)(1)')
 
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=2)
